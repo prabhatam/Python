65af0f8a6b2cdaae4cbb748170540bedc69de9df
Prabhat Gupta <136306864+prabhatam@users.noreply.github.com>
2024-09-12 01:11:12 +0530

    lecture 4 completed

--- 01_basics/chai.py
+++ 01_basics/chai.py
@@ -1,2 +1,14 @@
 from hello_chai import chai
 chai("ginger tea")
+
+
+"""
+Python is an interpreted language because the source code is converted into bytecode 
+and then executed by the Python virtual machine, instead of being built and linked like other compiled languages: 
+Python interpreter: A computer program that converts each line of high-level program code into machine code. 
+Bytecode: A platform-independent file that allows Python programs to run on any system with a compatible interpreter. 
+Python virtual machine: Executes the bytecode. 
+Python is also a hybrid language because it uses both a compiler and an interpreter for translation. 
+When Python code is run, it's first compiled and then interpreted line by line. 
+The compiled part is deleted after the code is executed. 
+"""
--- 01_basics/hello_chai.py
+++ 01_basics/hello_chai.py
@@ -4,3 +4,9 @@ def chai(n):
     print(n)
 
 chai("tea")
+
+
+# variables are also called attributes
+chai_one = "lemon tea"
+chai_two = "ginger tea"
+chai_three = "masala chai"
