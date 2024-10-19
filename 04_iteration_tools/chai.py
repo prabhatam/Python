b36ac9581c48fbcc206b98a53a59719454115eb6
Prabhat Gupta <136306864+prabhatam@users.noreply.github.com>
2024-10-19 15:52:40 +0530

    completed loops lecture 15

---
+++ 04_iteration_tools/chai.py
@@ -0,0 +1,26 @@
+import time
+print("chai is here")
+username = "prabhat"
+print(username)
+
+# step1: open this file in the integrated terminal
+# step2: now lets explore the file related operations
+
+# to open the file
+# myfile = open('chai.py)
+
+
+# to read the file
+# myfile.readline() => this reads the file one line
+# to read the complete file keep on writing this line againa again
+# at the end you will receive empty string when no line is left
+
+# background knowledge how file responds to looping
+# myfile.__next__() => this reads the file one line
+# to read the complete file keep on writing this line againa again
+# at the end you will error to stop iteration when no line is left
+
+# the above operation can be done using for loop also
+# for line in open('chai.py'):
+#   print(line,end='')
+
